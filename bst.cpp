a38ac6c31617696274cb34b73111645202e9b3c7
gitsuha <getsuha@gmail.com>
2019-05-29 18:49:34 +0530

    bst.cpp modified

--- bst.cpp
+++ bst.cpp
@@ -10,9 +10,23 @@ public:
 	Node(int d, Node* l = NULL, Node* r = NULL) : data(d), left(l), right(r) {}
 };
 
-void inorder(const Node* root)
+void inorder(const Node* n)
 {
+	if (n != NULL)
+	{
+		inorder(n->left);
+		cout << n->data;
+		inorder(n->right);
+	}
+}
 
+int height(const Node* n)
+{
+	if (n == NULL)
+		return -1;
+	int leftHeight = height(n->left);
+	int rightHeight = height(n->right);
+	return max(leftHeight, rightHeight) + 1;
 }
 
 int main()
@@ -33,5 +47,11 @@ int main()
 	n2->left = new Node(4);
 	n2->right = new Node(6, NULL, new Node(7));
 
+	inorder(root);
+	cout << endl;
+	cout << height(root);
+	cout << endl;
+
+	system("pause");
 	return 0;
 }
