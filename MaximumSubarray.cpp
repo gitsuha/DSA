5e884aef6eacbe51245a2b3ed55fc5ef03e67ecc
Suhasis Saha <33750161+gitsuha@users.noreply.github.com>
2019-06-25 17:33:38 +0530

    Add files via upload

---
+++ MaximumSubarray.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int getMaxSum_BF(vector<int>& a)
+{
+	int sum = 0;
+	for (size_t i = 0; i < a.size(); i++)
+	{
+		int localSum = 0;
+		for (size_t j = i; j < a.size(); j++)
+		{
+			localSum += a[j];
+			sum = max(sum, localSum);
+		}		
+	}
+	return sum;
+}
+
+int main()
+{
+	vector<int> a = { -2,1,-3,4,-1,2,1,-5,4 };
+	cout << getMaxSum_BF(a) << endl;
+	system("pause");
+	return 0;
+}
