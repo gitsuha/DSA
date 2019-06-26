a0b65932e1b0c870ec0a9da42973bc119c8fca02
gitsuha <getsuha@gmail.com>
2019-06-26 18:52:40 +0530

    Kadanes impl

--- MaximumSubarray.cpp
+++ MaximumSubarray.cpp
@@ -19,10 +19,23 @@ int getMaxSum_BF(vector<int>& a)
 	return sum;
 }
 
+int getMaxSum_Kadanes(vector<int>& a)
+{
+	int localSum = 0, globalSum = INT_MIN;
+	for (size_t i = 0; i < a.size(); i++)
+	{
+		localSum = max(a[i], a[i] + localSum);
+		globalSum = max(localSum, globalSum);
+	}
+	return globalSum;
+}
+
 int main()
 {
 	vector<int> a = { -2,1,-3,4,-1,2,1,-5,4 };
 	cout << getMaxSum_BF(a) << endl;
+	cout << getMaxSum_Kadanes(a) << endl;
+
 	system("pause");
 	return 0;
 }
