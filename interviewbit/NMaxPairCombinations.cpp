09c393d61fd22043fb6fa45ed32c6430557cb00d
Suhasis Saha <33750161+gitsuha@users.noreply.github.com>
2019-07-08 15:58:15 +0530

    interviewbit.com/problems/n-max-pair-combinations/

---
+++ interviewbit/NMaxPairCombinations.cpp
@@ -0,0 +1,42 @@
+#include "header.h"
+
+vector<int> solve(vector<int> &A, vector<int> &B) {
+	priority_queue<pair<int, pair<int, int> > > hp;
+	set<pair<int, int> > S;
+	int n = A.size();
+	sort(A.begin(), A.end());
+	sort(B.begin(), B.end());
+
+	hp.push(make_pair(A[n - 1] + B[n - 1], make_pair(n - 1, n - 1)));
+	S.insert(make_pair(n - 1, n - 1));
+
+	vector<int> ans;
+	int k = n;
+	while (k--) {
+		pair<int, pair<int, int> > top = hp.top();
+		hp.pop();
+		ans.push_back(top.first);
+		int L = top.second.first;
+		int R = top.second.second;
+
+		if (R > 0 && L >= 0 && S.find(make_pair(L, R - 1)) == S.end()) {
+			hp.push(make_pair(A[L] + B[R - 1], make_pair(L, R - 1)));
+			S.insert(make_pair(L, R - 1));
+		}
+		if (R >= 0 && L > 0 && S.find(make_pair(L - 1, R)) == S.end()) {
+			hp.push(make_pair(A[L - 1] + B[R], make_pair(L - 1, R)));
+			S.insert(make_pair(L - 1, R));
+		}
+	}
+	return ans;
+}
+
+int main()
+{
+	vector<int> a = { 1,4,2,3 };
+	vector<int> b = { 2,5,1,6 };
+	printVector(solve(a, b));
+
+	system("pause");
+	return 0;
+}
