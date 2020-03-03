bd108881bc198533d6ce7b088ec34c28fa51c591
Suhasis Saha <33750161+gitsuha@users.noreply.github.com>
2020-03-03 09:34:29 +0530

    Basic Calculator Leetcode
    https://leetcode.com/problems/basic-calculator-ii/
    https://leetcode.com/problems/basic-calculator-iii/
    
    Thanks to,
    https://www.youtube.com/watch?v=C_jxn1hTn6Q&feature=youtu.be
    https://happygirlzt.com/code/224.html

---
+++ cpp/calculator.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <queue>
+#include <string>
+
+using namespace std;
+
+int calculate(queue<char>& q) {
+	int curr_num = 0, prev_num = 0, res = 0;
+	char prev_op = '+';
+
+	while (!q.empty())
+	{
+		char c = q.front();
+		q.pop();
+		
+		if (c >= '0' && c <= '9') {
+			curr_num += curr_num * 10 + c - '0';
+		}
+		else if (c == '(') {
+			curr_num = calculate(q);
+		}
+		else {
+			switch (prev_op)
+			{
+			case '+':
+				res += prev_num;
+				prev_num = curr_num;
+				break;
+			case '-':
+				res += prev_num;
+				prev_num = -curr_num;
+				break;
+			case '*':
+				prev_num *= curr_num;
+				break;
+			case '/':
+				prev_num /= curr_num;
+				break;
+			default:
+				break;
+			}
+			if (c == ')') break;
+			curr_num = 0;
+			prev_op = c;
+		}
+	}
+	return res + prev_num;
+}
+
+int calculate(string s) {
+	queue<char> q;
+	// lets put the string into a queue discarding the spaces
+	for (char c : s) {
+		if (c != ' ')
+			q.push(c);
+	}
+
+	// pushing a space in the end to consider the last calc
+	q.push(' ');
+
+	return calculate(q);
+}
+
+int main() {
+
+	int n = calculate("(3+(4-2))* 2");
+	return 0;
+}
