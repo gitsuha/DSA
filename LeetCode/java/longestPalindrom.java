fda4d6cce2f2edf55823d655e873f55bb455efc0
Suhasis Saha <33750161+gitsuha@users.noreply.github.com>
2020-03-12 11:37:12 +0530

    Create longestPalindrom.java

---
+++ LeetCode/java/longestPalindrom.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String longestPalindrome(String input) {
+        if(input.isEmpty()) {
+          return "";
+        }
+        int n = input.length();
+        int longestSoFar = 0, startIndex = 0, endIndex = 0;
+        boolean[][] palindrom = new boolean[n][n];
+
+        for(int j = 0; j < n; j++) {
+          palindrom[j][j] = true;
+          for(int i = 0; i < j; i++) {
+            if(input.charAt(i) == input.charAt(j) && (j-i <= 2 || palindrom[i+1][j-1])) {
+              palindrom[i][j] = true;
+              if(j-i+1 > longestSoFar) {
+                longestSoFar = j-i+1;
+                startIndex = i;
+                endIndex = j;
+              }  
+            }
+          }
+        }
+        return input.substring(startIndex, endIndex+1);
+    }
+}
